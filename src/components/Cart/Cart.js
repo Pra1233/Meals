97719a2fd6ac2d2532f66f2e06d2d125275bffde
Prabhat <103235389+Pra1233@users.noreply.github.com>
2023-11-14 17:12:14 +0530

    Making a hard Coded Cart without any Functionality
    Task 5

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[
+        { id: "1", name: "Suci", amount: 100 },
+        { id: "2", name: "Prabhat", amount: 200 },
+      ].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>23</span>
+      </div>
+
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
